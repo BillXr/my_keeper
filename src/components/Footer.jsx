f7dcc8e1e28df0dd46fee82a09a8d536fdc5c501
Bill Xristodoulou <billchr456@gmail.com>
2023-08-13 17:41:25 +0300

    add changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
 
     return(
         <footer>
-            <p>Copyright ⓒ {currentYear}</p>
+            <p>Copyright ⓒ Vasilis Christodoulou {currentYear}</p>
         </footer>
     );
 }
